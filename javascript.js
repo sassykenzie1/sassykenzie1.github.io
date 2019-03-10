0bc57486bebb7b3dbb5a92cdd38015570f38f437
Makenzie <48410612+sassykenzie1@users.noreply.github.com>
2019-03-10 10:36:05 -0400

    Kink

---
+++ javascript.js
@@ -0,0 +1,36 @@
+var quotes= [
+  'Asphyxiation',
+  'Blindfolds',
+  'Bondage',
+  'Breath Play',
+  'Electric Play',
+  'Exhibitionism',
+  'Gags',
+  'Intercrural',
+  'Knife Play',
+  'Latex/Rubber',
+  'Masochism',
+  'Odaxelagnia',
+  'Omorashi',
+  'Orgasm Denial',
+  'Sadism',
+  'Sensation Play',
+  'Sensory Deprivation',
+  'Spanking',
+  'Stigmatophilia',
+  'Stockings',
+  'Suspension',
+  'Technophilia',
+  'Telephonicophilia',
+  'Uniforms',
+  'Vincilagnia',
+  'Voyeurism',
+  'Watersports (Urophilia)',
+  'Wax Play',
+  'Weapon Fetish',
+]
+
+function newQuote() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
