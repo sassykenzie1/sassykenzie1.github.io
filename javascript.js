15e4bd814dddc59b775e1d5cfef8a63229c77ab9
Makenzie <48410612+sassykenzie1@users.noreply.github.com>
2019-03-11 18:15:11 -0400

    Adjusted the toggle button

--- javascript.js
+++ javascript.js
@@ -37,9 +37,9 @@ function newQuote() {
 
 function myFunction() {
   var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
+  if (x.style.display = "none") {
     x.style.display = "block";
   } else {
-    x.style.display = "none";
+    x.style.display = "block";
   }
 }
