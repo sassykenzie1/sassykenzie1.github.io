22591a2eec54dfa67b824be0ca3bb9aeb78d6cd7
Makenzie <48410612+sassykenzie1@users.noreply.github.com>
2020-08-12 01:29:47 -0400

    Testing something different

--- javascript.js
+++ javascript.js
@@ -42,3 +42,13 @@ function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotes.length));
   document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
